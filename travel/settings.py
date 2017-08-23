e73b52e29dd5164b5eb5861ab778d080e0654a48
Windson <wiwindson@outlook.com>
2017-08-23 10:53:52 +0800

    add base settings and url for api

--- travel/settings.py
+++ travel/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'country.apps.CountryConfig',
+    'category.apps.CategoryConfig',
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
--- travel/urls.py
+++ travel/urls.py
@@ -1,21 +1,40 @@
-"""travel URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework import routers, viewsets
+from app.models import App
+from app.serializers import AppSerializer
+from category.models import Category
+from category.serializers import CategorySerializer
+from country.models import Country
+from country.serializers import CountrySerializer
+from .permissions import IsAdminOrReadOnly
+
+
+class AppViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminOrReadOnly,)
+    queryset = App.objects.all()
+    serializer_class = AppSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminOrReadOnly,)
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CountryViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAdminOrReadOnly,)
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+router = routers.DefaultRouter()
+router.register(r'app', AppViewSet)
+router.register(r'category', CategoryViewSet)
+router.register(r'country', CountryViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(router.urls)),
+    url(r'^api-auth/', include(
+        'rest_framework.urls', namespace='rest_framework'))
 ]
