0d33abb9e6c4235e978b55db754cefcb99e5bf75
Windson <wiwindson@outlook.com>
2017-09-13 11:39:17 +0800

    fix thunmbail type

--- category/models.py
+++ category/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     position = models.IntegerField(default=0)
-    thumnails = models.URLField()
+    thumnails = models.CharField(max_length=100)
     create_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
 
